2f6df341f29ae1ed9bfc32e034d12b2ee6407b62
saurabhtauke <saurabh.talele@students.iiserpune.ac.in>
2016-10-31 15:35:07 +0530

    Update randomwalk.m

--- Main/randomwalk.m
+++ Main/randomwalk.m
@@ -1,9 +1,28 @@
+clc
+
+% SaurabhTauke
+
+%% initialization Values
+
+N_monomer = 10000;               % Number of monomer units
+
+pos = zeros(N_monomer,3);        %array of position coordinates stored as X,Y,Z
+velocity = zeros(N_monomer,3);           %array of velocities stored as VX,VY,VZ
+force = zeros(N_monomer-1,3);              %force between any two non consecutive particles
+
+
+f_link = zeros(N_monomer-1,3);             %force between two consecutive particles
+
+dt = 0.0001;                                         % time step
+iter = 10000;                                        % number of iterations in simulation
+
+total_energy = zeros(iter,1);    % pre-allocated ... can be allocated in the force iteration loop tho.
+
 %% 3D random walk 
 
-pos = zeros(10000,3);
-r = zeros(9999,1);
+r = zeros(N_monomer-1,1);
 
-for i = 2:10000;
+for i = 2 : N_monomer;
     
     u1 = (rand()*2) -1;
     u2 = (rand()*2) -1;
@@ -17,10 +36,9 @@ for i = 2:10000;
     pos(i,2) = pos(i-1,2) + (2*u2 * sqrt(1 - (u1^2 + u2^2)));
     pos(i,3) = pos(i-1,3) + (1 - 2*(u1^2 + u2^2));
     
-    p = pos(i,1)^2 + pos(i,2)^2 + pos(i,3)^2;
-    q = pos(i-1,1)^2 + pos(i-1,2)^2 + pos(i-1,3)^2;
+    r(i-1) = (pos(i,1)-pos(i-1,1))^2 + (pos(i,2)-pos(i-1,2))^2 + (pos(i,3)-pos(i-1,3))^2 ;
     
-    r(i-1) = p-q;
+    
 end
 
 plot3(pos(:,1),pos(:,2),pos(:,3),'.')
